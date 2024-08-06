917c3b59a9d9d21cc4046722f3b7ff98b88e7ad9
Noah Ewell <noahewell.life@gmail.com>
2024-08-06 00:51:12 -0600

    Need to try queue over pointer approach

--- src/merge88/MergeSortedArray88.java
+++ src/merge88/MergeSortedArray88.java
@@ -10,20 +10,33 @@ class Solution {
         }
         else {
         	int[] temp = nums1.clone();
-        	int p1 = 0;
-        	int p2 = 0;
-        	for (int i=0; i<m; i++) {
-        		if (p1 != p2) {
+        	int p1 = temp[0];
+        	int p2 = nums2[0];
+        	for (int i=0; i<m-1; i++) {
+        		if (p1 != p2 && p1 != 0) {
         			if (p1 < p2) {
         				nums1[i] = p1;
+        				p1 = temp[i+1];
         			}
         			else {
         				nums1[i] = p2;
+        				p2 = nums2[i+1];
+        			}
+        		}
+        		else if (p1 == 0){
+        			nums1[i+1] = p2;
+        			if (nums2[i-1] == nums2[n]) {
+        				p2 = nums2[i--];
+        			}
+        			if (temp[1+i] == m-1) {
+        				p1 = temp[1 + i++];
         			}
         		}
         		else {
         			nums1[i] = p1;
+        			p1 = temp[i+1];
         			nums1[i+1] = p2;
+        			p2 = nums2[i];
         		}
         	}
         }
